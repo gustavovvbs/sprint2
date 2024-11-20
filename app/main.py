640bf4d25f9142441324224e008846c07275329a
kaua <gustavovvbs@al.insper.edu.br>
2024-11-20 08:36:08 -0300

    fix cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, Blueprint, request, jsonify 
 from app.api.endpoints.auth import auth_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(auth_bp, url_prefix = '/auth')
 
 @app.route('/')

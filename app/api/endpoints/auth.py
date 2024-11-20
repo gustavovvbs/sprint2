995c1c18bbd96179534744aa33e52009f7ba3d09
kaua <gustavovvbs@al.insper.edu.br>
2024-11-20 08:53:29 -0300

    add logging

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify 
+from flask import Blueprint, request, jsonify, current_app
 from app.services.auth import AuthService
 from app.schemas.auth import UserCreateSchema, UserLoginSchema
 from app.db.mongo_client import get_db
@@ -9,25 +9,31 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/register', methods=['POST'])
 def register_user():
     try:
+        current_app.logger.info('Register endpoint called')
         auth_service = AuthService(get_db())
         user_data = UserCreateSchema(**request.json)
         auth_service.register(user_data.dict())
         return jsonify({'message': 'user created successfully'}), 201
 
     except ValidationError as e:
+        current_app.logger.error(f'Validation error: {str(e)}')
         return jsonify({'error': 'validation error', 'details': str(e)}), 400
 
     except Exception as e:
+        current_app.logger.error(f'Internal server error: {e}')
         return jsonify({'error': f'internal server error {e}'}), 500
 
 @auth_bp.route('/login', methods=['POST'])
 def login_user():
     try:
+        current_app.logger.info('Login endpoint called')
         auth_service = AuthService(get_db())
         user_data = UserLoginSchema(**request.json)
         payload = auth_service.login(user_data.email, user_data.password)
         return jsonify(payload), 200
     except ValidationError as e:
+        current_app.logger.error(f'Validation error: {str(e)}')
         return jsonify({'error': 'validation error', 'details': str(e)}), 400
     except Exception as e:
+        current_app.logger.error(f'Internal server error: {e}')
         return jsonify({'error': f'internal server error {e}'}), 500
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, Blueprint, request, jsonify 
 from app.api.endpoints.auth import auth_bp
 from flask_cors import CORS
@@ -5,6 +6,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(auth_bp, url_prefix = '/auth')
+
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
